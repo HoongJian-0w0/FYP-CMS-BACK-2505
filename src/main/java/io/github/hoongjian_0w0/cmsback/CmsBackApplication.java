fa38c94e46cda163ffcee2bc8c9260b4bd22d213
Hoong Jian <hoongjian101@gmail.com>
2025-05-27 13:05:03 +0800

    Initiate - Spring Base Environment Setup

---
+++ src/main/java/io/github/hoongjian_0w0/cmsback/CmsBackApplication.java
@@ -0,0 +1,13 @@
+package io.github.hoongjian_0w0.cmsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/hoongjian_0w0/cmsback/CmsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hoongjian_0w0.cmsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
